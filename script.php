49efafbfe6e3456e95c3aa7c853328823ea880bf
mariocstweb <mariocstmra@gmail.com>
2024-02-16 16:42:15 +0100

    completed task

--- script.php
+++ script.php
@@ -1,2 +1,9 @@
 <?php
+$paragraph = $_GET['paragraph'];
+$word = $_GET['word'];
+echo "<p>$paragraph</p>";
+$new_paragraph = str_replace($word, '***', $paragraph);
+$paragraph_lenght = strlen($new_paragraph);
+echo "<p>$new_paragraph</p>";
+echo "<p>$paragraph_lenght</p>";
 ?>
